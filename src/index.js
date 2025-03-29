a165c7e67b489b48c57301e1121a952876a2fd4d
Alenia24 <112526474+Alenia24@users.noreply.github.com>
2025-03-29 01:02:12 -0400

    Weekly Spotlight Partial Complete

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 let bannerImg = document.querySelector(".bannerImg");
 let imagesAll = document.querySelector(".imagesAll");
 const favoriteBreeds = document.querySelector(".favoriteBreeds");
+const spotlight = document.querySelector(".spotlight");
 
 async function initialLoad() {
   const response = await axios.get("https://registry.dog/api/v1");
@@ -40,14 +41,14 @@ async function createBreeds() {
       data.id === "labrador-retriever"
     ) {
       const breedDiv = document.createElement("div");
-      breedDiv.classList.add("breedDiv")
+      breedDiv.classList.add("breedDiv");
       const img = document.createElement("img");
-      img.classList.add("img-fluid")
+      img.classList.add("img-fluid");
       img.setAttribute("src", data.images.small.indoors);
       const breedHeader = document.createElement("h4");
-      breedHeader.textContent=`${data.general.name}`
+      breedHeader.textContent = `${data.general.name}`;
       breedDiv.appendChild(img);
-      breedDiv.appendChild(breedHeader)
+      breedDiv.appendChild(breedHeader);
       favoriteBreeds.append(breedDiv);
     }
   });
@@ -55,5 +56,34 @@ async function createBreeds() {
   console.log(breeds);
 }
 
+async function createSpotlight() {
+  const response = await axios.get("https://registry.dog/api/v1");
+  let breeds = response.data.data;
+
+  breeds.forEach((data) => {
+    if (data.id === "maltipoo") {
+      const img = document.createElement("img");
+      img.classList.add("img-fluid");
+      img.setAttribute("src", data.images.small.indoors);
+      const spotlightIntro = document.createElement("div");
+      spotlightIntro.classList.add("spotlightIntro");
+      const spotlightHeader = document.createElement("h2");
+      spotlightHeader.textContent = `Meet Spark`;
+      const spotlightDescr = document.createElement("p");
+      spotlightDescr.textContent = `${data.general.shortDescription}`;
+      const adoptBtn = document.createElement("button");
+      adoptBtn.classList.add("adoptBtn");
+      const text = document.createTextNode("Adopt Now");
+      adoptBtn.appendChild(text);
+      spotlight.appendChild(img);
+      spotlight.appendChild(spotlightIntro);
+      spotlightIntro.appendChild(spotlightHeader);
+      spotlightIntro.appendChild(spotlightDescr);
+      spotlightIntro.appendChild(adoptBtn);
+    }
+  });
+}
+
+createSpotlight();
 createBody();
 createBreeds();
